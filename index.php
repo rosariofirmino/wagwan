b59ac3bb8b7cef1b8c0fd5ba02245e74d64c5a64
sebastiandm2 <s.martinez1@ufl.edu>
2023-05-01 15:14:16 -0400

    actually comment it out

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 #!/usr/local/bin/php
 <?php
 
-session_start();
+// session_start();
 
-// Check if the user is already logged in
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: ./php/login.php");
-    exit;
-}
+// // Check if the user is already logged in
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//     header("location: ./php/login.php");
+//     exit;
+// }
 
 ?>
 <style>
