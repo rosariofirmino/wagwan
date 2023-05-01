a9f5b296b1468cb10a9d58bc1cbc64706ad88423
sebastiandm2 <s.martinez1@ufl.edu>
2023-05-01 14:29:27 -0400

    fix session redirect

--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@ session_start();
  
 // Check if the user is already logged in
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: ../index.php");
     exit;
 }
 
