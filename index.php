9f6b18dffc4a8056dd3e49dc734e48b3030c7229
sebastiandm2 <s.martinez1@ufl.edu>
2023-05-01 15:12:55 -0400

    require session loggedin key - commented out for dev

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 #!/usr/local/bin/php
+<?php
+
+session_start();
+
+// Check if the user is already logged in
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ./php/login.php");
+    exit;
+}
+
+?>
 <style>
 * {
 	font-family: 'Montserrat', sans-serif;
