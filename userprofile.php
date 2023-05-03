089cf1b095f3246d406542d8df3597287e6523cf
dpayne77 <dpayne1@ufl.edu>
2023-05-03 09:31:32 -0400

    Removed broken and unused fontawesome link

--- userprofile.php
+++ userprofile.php
@@ -137,7 +137,6 @@ require_once("postprinter.php");
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<script src="https://kit.fontawesome.com/your_code.js" crossorigin="anonymous"></script>
 <script src="js/functions.js"></script>
 
 <script>
