128129cc5b582f3f6414c7055067de03ab2f2415
dpayne77 <dpayne1@ufl.edu>
2023-05-02 23:07:54 -0400

    Fixed session and likes

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,5 @@
 function likeButtonPress(Row, PostId, Likes, UserLiked) {
 
-
-
     if(UserLiked == true) { // unlike
         // set icon to unlike
         $("#" + Row + "path" + PostId).attr("d", "m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z");
@@ -30,16 +28,18 @@ function likeButtonPress(Row, PostId, Likes, UserLiked) {
     
     // AJAX for like update / update like in database
     
-    // get UserId
-    var UserId = "admin"; // TODO: get UserId from session
+    // get UserId from session in 'id'
     
     const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        id = id;
+    }
     
     if (UserLiked == true) { // unlike
-        xhttp.open("GET", "actions/dislike.php?PostId=" + PostId + "&UserId=" + UserId, true);
+        xhttp.open("GET", "actions/dislike.php?PostId=" + PostId + "&UserId=" + id, true);
     }
     if (UserLiked == false) { // like
-        xhttp.open("GET", "actions/like.php?PostId=" + PostId + "&UserId=" + UserId, true);
+        xhttp.open("GET", "actions/like.php?PostId=" + PostId + "&UserId=" + id, true);
     }
     
     xhttp.send();
